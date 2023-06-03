7670b8132bca5097fa2c00c5999e0b4024f4b16c
pedrocerqueiras <pedrosilvap2@hotmailcom>
2023-06-03 17:16:25 -0300

    - transformando automaticamente para metros

--- app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,40 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular: Button = findViewById(R.id.btnCalcular)
         val edtPeso: EditText = findViewById(R.id.edtPeso)
         val edtAltura: EditText = findViewById(R.id.edtAltura)
+
+        edtAltura.addTextChangedListener(object : TextWatcher {
+            private var isEditing: Boolean = false
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Nada a fazer aqui
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Nada a fazer aqui
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (isEditing) return
+
+                isEditing = true
+
+                // Remove qualquer ponto existente
+                val textWithoutDots = s.toString().replace(".", "")
+
+                // Verifica se há pelo menos um número antes do ponto
+                if (textWithoutDots.isNotEmpty()) {
+                    val intValue = textWithoutDots.toInt()
+                    val formattedValue = intValue / 100.toFloat()
+
+
+                    // Atualiza o texto com o ponto no lugar adequado
+                    edtAltura.setText(String.format("%.2f", formattedValue))
+                    edtAltura.setSelection(edtAltura.text.length)
+                }
+
+                isEditing = false
+            }
+        })
 
         btnCalcular.setOnClickListener {
 
